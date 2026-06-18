7acda66516034548f7755daf9d3a1b9384984a92
HalaData <info.hala@yandex.com>
2026-06-18 20:00:41 +0200

    Create __init__.py for Empowerment module

---
+++ modules/Empowerment/-init_ .py
@@ -0,0 +1,6 @@
+# Empowerment Module for GOE OS
+from .engine import EmpowermentEngine
+from .app_generator import AppGenerator
+from .wizard import Wizard
+
+__all__ = ["EmpowermentEngine", "AppGenerator", "Wizard"]
