d93a45c709fc33f9dbf76c05306febf5ff6af478
HalaData <info.hala@yandex.com>
2026-06-25 15:32:32 +0200

    Enhance Core module with documentation and imports
    Added module documentation in Arabic and imported various system components.

--- Core/_Init_.py
+++ Core/_Init_.py
@@ -1,1 +1,20 @@
-# GOE OS Core Module
+"""
+مجلد الأنظمة المطورة - يحتوي على جميع المكونات الاستراتيجية والتشغيلية
+"""
+
+from .reputation import ReputationSystem
+from .challenges import ChallengeSystem
+from .templates import TemplateSystem
+from .mentors import GovernanceMentorSystem
+from .gaps import TransparentGapLayer
+from .community import GOECommunity
+from .strategic_simulator import StrategicSimulator
+from .clarity_index import CompetitiveClarityIndex
+from .clear_path import ClearPathMap
+from .demand_discovery import DemandDiscoveryEngine
+from .specialization_matrix import HyperSpecializationMatrix
+from .advantage_generator import UnignorableAdvantageGenerator
+from .sustainability_analyzer import CompetitiveSustainabilityAnalyzer
+from .auto_loader_v2 import AutoLoaderV2
+from .integration import IntegrationLayer
+from .schemaless_data import SchemalessDataLayer
